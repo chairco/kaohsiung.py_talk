f3c5bd16b13301e07425e249598873eaded08948
chairco <chairco@gmail.com>
2018-06-13 14:37:04 +0800

    Add server.py

---
+++ 20180613_python_coroutine_work/server.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from time import sleep
+
+from flask import Flask, Response
+
+
+app = Flask(__name__)
+
+
+message = b'Hello PyConTW2018! ' * 100 + b'\n'
+CHUNK_LEN = 100
+N_CHUNKS = len(message) / CHUNK_LEN
+
+@app.route("/foo")
+@app.route("/bar")
+def hello():
+    def generate():
+        i = 0
+        while True:
+            chunk = message[i:i + CHUNK_LEN]
+            if chunk:
+                yield chunk
+                sleep(0.877 / N_CHUNKS)
+                i += CHUNK_LEN
+            else:
+                break
+
+    return Response(generate())
+
+
+@app.route("/test")
+def quick():
+    return 'Here is /test'
+
+
+if __name__ == "__main__":
+    app.run(threaded=True)
