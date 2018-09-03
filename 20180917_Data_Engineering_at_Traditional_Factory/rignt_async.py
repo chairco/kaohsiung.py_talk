a248eea65ed9248c83fc14e7ee50c0b7e4b86941
chairco <chairco@gmail.com>
2018-09-03 17:09:58 +0800

    Add new talk sample code

---
+++ 20180917_Data_Engineering_at_Traditional_Factory/rignt_async.py
@@ -0,0 +1,61 @@
+#-*- coding: utf-8 -*-
+import asyncio
+import random
+import time
+
+
+async def gen(num):
+    i = 0
+    while True:
+        yield i
+        i += 1
+        if i == num:
+            break
+        # time.sleep(0.5)
+        if i == 5:
+            print('sleep 5s')
+            await asyncio.sleep(5) # async sleep
+            print('end sleep')
+
+
+async def produce(queue, n):
+    start = time.time()
+    async for x in gen(n + 1):
+        item = str(x)
+        # produce an item
+        print(f"producing {item}/{n}, id:{id(item)}")
+        # simulate i/o operation using sleep
+        await asyncio.sleep(random.random())
+        # put the item in the queue
+        await queue.put(item)
+
+
+async def consume(queue):
+    while True:
+        # wait for an item from the producer
+        item = await queue.get()
+
+        # process the item
+        print(f"consuming {item}..., id:{id(item)}")
+        # simulate i/o operation using sleep
+        await asyncio.sleep(random.random())
+
+        # Notify the queue that the item has been processed
+        queue.task_done()
+
+
+async def run(n):
+    queue = asyncio.Queue()
+    # schedule the consumer
+    consumer = asyncio.ensure_future(consume(queue))
+    # run the producer and wait for completion
+    await produce(queue, n)
+    # wait until the consumer has processed all items
+    await queue.join()
+    # the consumer is still awaiting for an item, cancel it
+    consumer.cancel()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run(5))
+loop.close()
